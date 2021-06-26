a2d1eb817c458f9bd04320a5b84f508518c663cb
Keerthana0025 <86304424+Keerthana0025@users.noreply.github.com>
2021-06-26 20:09:02 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Footer(){
+  return(
+    <p>
+      <footer>Copyrights by ShapeAI @{new Date().getFullYear()}</footer>
+    </p>
+
+  );
+}
+export default Footer;
